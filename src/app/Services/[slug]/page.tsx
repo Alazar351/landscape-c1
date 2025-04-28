48a41656fac7d61fa8ac75bf280d6d2a364fc4d9
Alazar351 <alazar.lema351@gmail.com>
2025-04-28 14:02:31 -0500

    ts error

--- src/app/Services/[slug]/page.tsx
+++ src/app/Services/[slug]/page.tsx
@@ -5,11 +5,17 @@ import Links from "./_component/Links";
 import { details } from "@/data/services";
 import { notFound } from "next/navigation";
 
+interface DetailsPageProps {
+  params: {
+    slug: string;
+  };
+}
+
 export async function generateStaticParams() {
   return details.map(({ id }) => ({ slug: id }));
 }
 
-export default function Details({ params }: { params: { slug: string } }) {
+export default function Details({ params }: DetailsPageProps) {
   const detail = details.find((d) => d.id === params.slug);
 
   if (!detail) {
